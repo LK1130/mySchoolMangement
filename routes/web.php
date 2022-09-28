531d22b034d02ae4b25c14fbb9a5cf3271311050
Aung Min Khant <aungmk299@gmail.com>
2022-09-28 09:12:31 +0630

    no message

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,8 @@ use Inertia\Inertia;
 |
 */
 
-Route::get('/', function () {
+
+Route::get('/',function(){
     return Inertia::render('Welcome', [
         'canLogin' => Route::has('login'),
         'canRegister' => Route::has('register'),
@@ -24,13 +25,6 @@ Route::get('/', function () {
         'phpVersion' => PHP_VERSION,
     ]);
 });
-
-// Route::get('/',function(){
-//     return Inertia::render('Login'
-    
-// );
-// });
-
     
 
 
