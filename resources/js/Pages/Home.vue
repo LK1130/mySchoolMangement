17c7b0bfe23e8af9c51e4de629ed5ab2768a9363
Yan Naing Htwe <yannainghtwe.777.yy@gmail.com>
2022-10-26 16:44:14 +0630

    Updated Necessary Essential Responsive (Home, Inbox, Video Pages)

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -7,20 +7,6 @@ import Footer from '../Layouts/Footer.vue';
 import Chart from '../Components/LineChart.vue';
 import ChartV2 from '../Components/LineChartV2.vue'
 
-// Import Swiper Vue.js components
-import { Swiper, SwiperSlide } from "swiper/vue";
-
-// Import Swiper styles
-import "swiper/css";
-
-import "swiper/css/navigation";
-import "swiper/css/pagination";
-
-// import "./style.css";
-
-// import required modules
-import { Navigation, Pagination, Mousewheel, Keyboard } from "swiper";
-
 const chartOptions = ref({
     chart: {
         toolbar: {
@@ -112,57 +98,51 @@ const stroke = ref([
         </div>
 
         <!-- Hero Section -->
-        <div class="flex flex-col lg:flex-row justify-between items-center py-4 w-full ">
+        <div class="flex flex-col xl:flex-row justify-between items-center py-4 w-full ">
             <!-- Student's Card -->
 
-            <div class="p-10 w-full">
-                <swiper :cssMode="true" :navigation="true" :pagination="{ clickable: true }" :mousewheel="true"
-                    :keyboard="true" :modules="modules">
-                    <swiper-slide>
-                        <div
-                            class="flex flex-col rounded-xl bg-primaryBackground w-full space-y-14 text-white p-8 w-full mb-4 md:mb-0 overflow-hidden card">
-                            <div class="flex flex-row justify-between items-center z-10">
-                                <div class="flex flex-col space-y-3">
-                                    <h1>Linn Ko Ko</h1>
-                                    <p>N5 > Batch 18</p>
-                                    <div class="flex flex-row">
-                                        <div class="w-12 h-2 bg-secondaryBackground rounded-tl-md rounded-bl-md"></div>
-                                        <div class="w-12 h-2 bg-secondaryBackground mx-2"></div>
-                                        <div class="w-12 h-2 bg-white rounded-tr-md rounded-br-md"></div>
-                                    </div>
-                                </div>
-                                <div class="relative flex flex-row items-center justify-center">
-                                    <svg width="50" height="60" viewBox="0 0 114 130" fill="none"
-                                        xmlns="http://www.w3.org/2000/svg">
-                                        <path
-                                            d="M33.21 4.55C42.0257 2.15313 51.3938 0 57 0C62.6063 0 71.9744 2.15313 80.79 4.55C89.8088 6.9875 98.9007 9.87187 104.247 11.6187C106.482 12.3569 108.464 13.7107 109.964 15.5244C111.465 17.3382 112.423 19.5385 112.729 21.8725C117.572 58.2481 106.335 85.2069 92.7013 103.041C86.9199 110.67 80.0262 117.389 72.2507 122.972C69.562 124.904 66.7133 126.603 63.7357 128.05C61.4607 129.122 59.015 130 57 130C54.985 130 52.5475 129.122 50.2644 128.05C47.2867 126.603 44.438 124.904 41.7494 122.972C33.974 117.389 27.0804 110.67 21.2988 103.041C7.66504 85.2069 -3.57183 58.2481 1.27067 21.8725C1.57688 19.5385 2.53527 17.3382 4.03574 15.5244C5.53622 13.7107 7.51791 12.3569 9.75317 11.6187C17.5141 9.07407 25.3353 6.71714 33.21 4.55Z"
-                                            fill="#FFC652" />
-                                    </svg>
-                                    <p class="text-3xl font-bold rank">10</p>
-                                </div>
+            <div class="p-10 lg:w-1/2 w-full">
+                <div
+                    class="flex flex-col rounded-xl bg-primaryBackground w-full space-y-14 text-white p-8 w-full mb-4 md:mb-0 overflow-hidden card">
+                    <div class="flex flex-row justify-between items-center z-10">
+                        <div class="flex flex-col space-y-3">
+                            <h1>Linn Ko Ko</h1>
+                            <p>N5 > Batch 18</p>
+                            <div class="flex flex-row">
+                                <div class="w-12 h-2 bg-secondaryBackground rounded-tl-md rounded-bl-md"></div>
+                                <div class="w-12 h-2 bg-secondaryBackground mx-2"></div>
+                                <div class="w-12 h-2 bg-white rounded-tr-md rounded-br-md"></div>
                             </div>
-
-                            <div class="flex flex-row justify-between z-10">
-                                <div class="flex flex-col space-y-4">
-                                    <p>Attendance > <span
-                                            class="ml-3 text-lg font-bold text-secondaryBackground">80%</span>
-                                    </p>
-                                    <p>Exam Mark > <span
-                                            class="ml-3 text-lg font-bold text-secondaryBackground">60%</span>
-                                    </p>
-                                </div>
-                                <p>Status > <span class="ml-3 text-lg font-bold">Active</span></p>
-                            </div>
-
-                            <p class="uppercase text-7xl md:text-9xl z-0 backTitle">Japanese</p>
                         </div>
-                    </swiper-slide>
+                        <div class="relative flex flex-row items-center justify-center">
+                            <svg width="50" height="60" viewBox="0 0 114 130" fill="none"
+                                xmlns="http://www.w3.org/2000/svg">
+                                <path
+                                    d="M33.21 4.55C42.0257 2.15313 51.3938 0 57 0C62.6063 0 71.9744 2.15313 80.79 4.55C89.8088 6.9875 98.9007 9.87187 104.247 11.6187C106.482 12.3569 108.464 13.7107 109.964 15.5244C111.465 17.3382 112.423 19.5385 112.729 21.8725C117.572 58.2481 106.335 85.2069 92.7013 103.041C86.9199 110.67 80.0262 117.389 72.2507 122.972C69.562 124.904 66.7133 126.603 63.7357 128.05C61.4607 129.122 59.015 130 57 130C54.985 130 52.5475 129.122 50.2644 128.05C47.2867 126.603 44.438 124.904 41.7494 122.972C33.974 117.389 27.0804 110.67 21.2988 103.041C7.66504 85.2069 -3.57183 58.2481 1.27067 21.8725C1.57688 19.5385 2.53527 17.3382 4.03574 15.5244C5.53622 13.7107 7.51791 12.3569 9.75317 11.6187C17.5141 9.07407 25.3353 6.71714 33.21 4.55Z"
+                                    fill="#FFC652" />
+                            </svg>
+                            <p class="text-3xl font-bold rank">10</p>
+                        </div>
+                    </div>
 
-                </swiper>
+                    <div class="flex flex-row justify-between z-10">
+                        <div class="flex flex-col space-y-4">
+                            <p>Attendance > <span class="ml-3 text-lg font-bold text-secondaryBackground">80%</span>
+                            </p>
+                            <p>Exam Mark > <span class="ml-3 text-lg font-bold text-secondaryBackground">60%</span>
+                            </p>
+                        </div>
+                        <p>Status > <span class="ml-3 text-lg font-bold">Active</span></p>
+                    </div>
+
+                    <p class="uppercase text-7xl md:text-9xl z-0 backTitle">Japanese</p>
+                </div>
             </div>
 
+
+
             <!-- Class Info -->
-            <div class="flex flex-col w-full px-10 md:pl-36">
+            <div class="flex flex-col px-10 xl:pl-36 lg:w-1/2 w-full">
                 <h1 class="text-lg md:text-xl font-bold text-primaryBackground">Class info</h1>
 
                 <div class="relative">
@@ -220,7 +200,7 @@ const stroke = ref([
                     </table>
                 </div>
 
-                <div class="w-full flex justify-end">
+                <div class="w-full flex justify-end mt-4">
                     <Link href="/recording">
                     <button
                         class="w-32 text-white bg-red-600 hover:bg-red-800 focus:outline-none font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2">Rec
@@ -232,7 +212,7 @@ const stroke = ref([
     </section>
 
     <!-- Rank Section -->
-    <div class="flex flex-col w-full bg-primaryBackground py-10 my-10">
+    <div class="flex flex-col w-full bg-primaryBackground py-14 my-10">
         <div class="flex flex-row justify-around w-full items-center">
             <div class="flex flex-col items-center">
                 <h1 class="text-2xl md:text-4xl text-secondaryBackground font-bold">80%</h1>
@@ -255,9 +235,9 @@ const stroke = ref([
             </div>
         </div>
 
-        <div class="flex flex-col lg:flex-row justify-around items-center space-y-5 px-10">
+        <div class="flex flex-col xl:flex-row justify-around items-center xl:space-y-0 space-y-5 my-28 xl:my-0 h-96">
             <!-- Rank Table -->
-            <div class="flex flex-col bg-white py-5 px-10 rounded-xl">
+            <div class="flex flex-col bg-white py-5 px-10 rounded-xl h-full">
                 <table class="text-sm text-left text-primaryBackground ">
                     <thead>
                         <tr class="text-black">
@@ -303,9 +283,9 @@ const stroke = ref([
             </div>
 
             <!-- Rank Chart -->
-            <div class="flex flex-col bg-white py-5 px-10 rounded-xl">
+            <div class="flex flex-col bg-white py-5 px-10 rounded-xl h-full">
                 <h1>Overall</h1>
-                <div class="w-full">
+                <div class="">
                     <Chart :options="chartOptions" :series="series" />
                 </div>
             </div>
@@ -367,18 +347,4 @@ const stroke = ref([
 @import "../../css/home.css";
 </style>
 
-<script>
 
-
-export default {
-    components: {
-        Swiper,
-        SwiperSlide,
-    },
-    setup() {
-        return {
-            modules: [Navigation, Pagination, Mousewheel, Keyboard],
-        };
-    },
-};
-</script>
