3670c126c840f2632e24c1c47aed2d685fd46b71
Aung Min Khant <aungmk299@gmail.com>
2022-11-01 18:28:05 +0630

    not complete yet

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\TStudentClass;
+use App\Models\TStudentExam;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -12,7 +13,9 @@ class HomeController extends Controller
     public function index()
     {
         $class = new TStudentClass();
+        $exam   = new TStudentExam();
+        $examRank = $exam->showRankTable();
         $totalClass = $class->totalClass(Auth::id());
-        return inertia("Home", ['classes' => $totalClass]);
+        return inertia("Home", ['classes' => $totalClass,'examRank' => $examRank]);
     }
 }
--- app/Models/TStudentExam.php
+++ app/Models/TStudentExam.php
@@ -2,10 +2,35 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class TStudentExam extends Model
 {
     use HasFactory;
+
+    public function showRankTable(){
+
+//         SELECT t_student_exams.date_submitted,m_exams.e_name, t_student_exams.mark  FROM `t_student_exams`
+// JOIN users ON `t_student_exams`.`user_id` = users.id
+// JOIN m_exams ON `t_student_exams`.`exam_id` = m_exams.id
+
+// WHERE t_student_exams.date_submitted < NOW()
+// GROUP BY t_student_exams.exam_id
+// ORDER BY t_student_exams.exam_id ,t_student_exams.mark DESC
+
+$query=    DB::select(
+    DB::raw("
+        SELECT t_student_exams.date_submitted,m_exams.e_name, t_student_exams.mark  FROM `t_student_exams`
+    JOIN users ON `t_student_exams`.`user_id` = users.id
+    JOIN m_exams ON `t_student_exams`.`exam_id` = m_exams.id
+    WHERE t_student_exams.date_submitted < NOW()
+    GROUP BY t_student_exams.exam_id
+    ORDER BY t_student_exams.exam_id ,t_student_exams.mark DESC"));
+   
+
+    return $query;
+    }
 }
