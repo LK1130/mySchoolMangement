ce1473312016e7ba3ffeb912a80b8307179dbe08
Yan Naing Htwe <yannainghtwe.777.yy@gmail.com>
2022-11-28 17:05:17 +0630

    404 darkmode updated

--- public/js/darkmode.js
+++ public/js/darkmode.js
@@ -2,6 +2,7 @@ const html = document.querySelector("html");
 const darkMode = stringToBoolean(localStorage.getItem("darkMode"));
 
     darkMode ? html.classList.add("dark") : html.classList.remove("dark");
+    
 function stringToBoolean(value) {
     return String(value) === "1" || String(value).toLowerCase() === "true";
 }
